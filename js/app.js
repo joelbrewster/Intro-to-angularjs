ff2402425c19a89266fb9f5b5c38a7ece9da170b
Joel Brewster <j.n.brewster@gmail.com>
2016-07-06 11:26:49 +1000

    App js :

---
+++ js/app.js
@@ -0,0 +1,15 @@
+/*global angular*/
+
+var app = angular.module('IntroToAngularApp', []);
+
+
+// function HomeController($scope){
+//   $scope.awesome = true;
+// }
+
+// function HomeController(){
+//   this.awesome = true;
+//   return this;
+// }
+
+app.controller('HomeController', HomeController);
